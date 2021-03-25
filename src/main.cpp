cb927df2e3e57294fe9eb483dc54b7918f117643
sunandbean <sunbin.vincent.kim@gmail.com>
2021-03-25 23:54:32 +0900

    gradle test

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char* argv[])
 {
-    // int n = atoi(argv[1]);
-    // auto factorial = Factorial::GetFactorial(n);
+    int n = atoi(argv[1]);
+    auto factorial = Factorial::GetFactorial(n);
 
-    // std::cout << "The Factorial of " << n << "=" << factorial << std::endl;
+    std::cout << "The Factorial of " << n << "=" << factorial << std::endl;
     std::cout << "The Factorial of 7 = 5040" << std::endl;
     std::cout << "The Factorial of 7 = 5040" << std::endl;
     std::cout << "The Factorial of 7 = 5040" << std::endl;
